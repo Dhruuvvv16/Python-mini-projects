8c7028bc93a80fe7b49e18e581445ef0b52faf93
Leon287 <dhruvjp04@gmail.com>
2026-06-25 10:36:40 +0530

    Update tictactoe.py

--- Tic Tac Toe/tictactoe.py
+++ Tic Tac Toe/tictactoe.py
@@ -3,6 +3,16 @@ import math
 EMPTY = " "
 
 
+def print_board(board):
+    print()
+    for i in range(0, 9, 3):
+        row = board[i:i + 3]
+        print(" | ".join(row))
+        if i < 6:
+            print("--+---+--")
+    print()
+
+
 
 if __name__ == "__main__":
     main()
