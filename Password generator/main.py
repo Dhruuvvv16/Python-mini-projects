4a345f2003d85543623abf75a5ec82dc08539479
Leon287 <dhruvjp04@gmail.com>
2026-06-26 01:18:50 +0530

    Update main.py

--- Password generator/main.py
+++ Password generator/main.py
@@ -4,5 +4,18 @@ import string
 
 SYMBOLS = "!@#$%^&*()-_=+[]{};:,.<>?"
 
+
+def build_charset(use_upper, use_lower, use_digits, use_symbols):
+    charset = ""
+    if use_upper:
+        charset += string.ascii_uppercase
+    if use_lower:
+        charset += string.ascii_lowercase
+    if use_digits:
+        charset += string.digits
+    if use_symbols:
+        charset += SYMBOLS
+    return charset
+
 if __name__ == "__main__":
     main()
