c6b4b0effb3b2ef5286bc1d92ab58a9223bcc263
Leon287 <dhruvjp04@gmail.com>
2026-06-28 00:34:32 +0530

    Update main.py

--- Flashcards/main.py
+++ Flashcards/main.py
@@ -3,6 +3,8 @@ import json
 import os
 import random
 
+DATA_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "flashcards.json")
+MAX_BOX = 5
 
 if __name__ == "__main__":
     main()
