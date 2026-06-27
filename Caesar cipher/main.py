ffe72f2f49ed175485cbf1143799bdc2af451463
Leon287 <dhruvjp04@gmail.com>
2026-06-27 11:28:39 +0530

    Update main.py

--- Caesar cipher/main.py
+++ Caesar cipher/main.py
@@ -3,5 +3,14 @@ import string
 ALPHABET = string.ascii_lowercase
 
 
+def shift_char(char, shift):
+    if char.islower():
+        index = (ALPHABET.index(char) + shift) % 26
+        return ALPHABET[index]
+    if char.isupper():
+        index = (ALPHABET.index(char.lower()) + shift) % 26
+        return ALPHABET[index].upper()
+    return char
+
 if __name__ == "__main__":
     main()
