1fbff19de74d0611d9bc36a95a59cb66d50aea80
Leon287 <dhruvjp04@gmail.com>
2026-06-29 14:39:07 +0530

    Update main.py

--- BaseConverter/main.py
+++ BaseConverter/main.py
@@ -2,5 +2,10 @@ import argparse
 
 BASES = {"bin": 2, "oct": 8, "dec": 10, "hex": 16}
 
+
+def to_decimal(value, base):
+    return int(value, BASES[base])
+
+
 if __name__ == "__main__":
     main()
