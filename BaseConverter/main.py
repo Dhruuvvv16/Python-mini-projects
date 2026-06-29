1cd4e085f22d18d918a7e1e8eecda2c1cdca9352
Leon287 <dhruvjp04@gmail.com>
2026-06-29 14:39:10 +0530

    Update main.py

--- BaseConverter/main.py
+++ BaseConverter/main.py
@@ -7,5 +7,14 @@ def to_decimal(value, base):
     return int(value, BASES[base])
 
 
+def from_decimal(value, base):
+    if base == "bin":
+        return bin(value)[2:]
+    if base == "oct":
+        return oct(value)[2:]
+    if base == "hex":
+        return hex(value)[2:]
+    return str(value)
+
 if __name__ == "__main__":
     main()
