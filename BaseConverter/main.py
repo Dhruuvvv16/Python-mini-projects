efc06e146b319b6b0d083241d97cd6ffa291c032
Leon287 <dhruvjp04@gmail.com>
2026-06-29 14:39:13 +0530

    Update main.py

--- BaseConverter/main.py
+++ BaseConverter/main.py
@@ -16,5 +16,10 @@ def from_decimal(value, base):
         return hex(value)[2:]
     return str(value)
 
+
+def convert(value, from_base, to_base):
+    decimal_value = to_decimal(value, from_base)
+    return from_decimal(decimal_value, to_base)
+
 if __name__ == "__main__":
     main()
