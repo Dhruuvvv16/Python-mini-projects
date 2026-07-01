24ab650774dc73a3bff1afdf9062854bff4edc91
Leon287 <dhruvjp04@gmail.com>
2026-07-01 07:29:05 +0530

    Update main.py

--- Image to ASCII/main.py
+++ Image to ASCII/main.py
@@ -3,5 +3,11 @@ import argparse
 from PIL import Image
 
 ASCII_CHARS = "@%#*+=-:. "
+def resize_image(image, new_width):
+    width, height = image.size
+    aspect_ratio = height / width
+    # 0.55 roughly compensates for terminal characters being taller than wide
+    new_height = max(int(new_width * aspect_ratio * 0.55), 1)
+    return image.resize((new_width, new_height))
 if __name__ == "__main__":
     main()
