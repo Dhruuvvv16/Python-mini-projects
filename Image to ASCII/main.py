36d85b028ae0054df7f04b30e915852ab41daa6e
Leon287 <dhruvjp04@gmail.com>
2026-07-01 07:29:13 +0530

    Update main.py

--- Image to ASCII/main.py
+++ Image to ASCII/main.py
@@ -16,5 +16,16 @@ def pixels_to_ascii(image):
     return "".join(ASCII_CHARS[pixel * (len(ASCII_CHARS) - 1) // 255] for pixel in pixels)
 
 
+def convert_image(path, width):
+    image = Image.open(path)
+    image = resize_image(image, width)
+    image = image.convert("L")  # grayscale
+
+    ascii_str = pixels_to_ascii(image)
+    img_width = image.width
+    rows = [ascii_str[i:i + img_width] for i in range(0, len(ascii_str), img_width)]
+    return "\n".join(rows)
+
+
 if __name__ == "__main__":
     main()
