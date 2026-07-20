d437e121baf8096480acc9aad676e4dcb5e8fc2b
Leon287 <dhruvjp04@gmail.com>
2026-07-20 19:06:54 +0530

    Update main.py

--- hnHeadlines/main.py
+++ hnHeadlines/main.py
@@ -2,5 +2,9 @@ import argparse
 import sys
 
 import requests
+
+TOP_STORIES_URL = "https://hacker-news.firebaseio.com/v0/topstories.json"
+ITEM_URL = "https://hacker-news.firebaseio.com/v0/item/{id}.json"
+
 if __name__ == "__main__":
     main()
